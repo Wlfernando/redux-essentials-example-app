ad254c9ca8ee3986545c8f799cc480d98cac8b14
Fernando Alcázar Malfavón <4maferal@gmail.com>
2026-09-12 17:18:30 -0600

    add usersSlice

---
+++ src/store/features/users/usersSlice.ts
@@ -0,0 +1,26 @@
+import { RootState } from "@/store";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const initialState: User[] = [
+  { id: '0', name: 'Tianna Jenkins' },
+  { id: '1', name: 'Kevin Grant' },
+  { id: '2', name: 'Madison Price' },
+];
+
+const userSlice = createSlice({
+  initialState,
+  name: 'users',
+  reducers: {
+
+  },
+});
+
+export default userSlice.reducer;
+
+const selectAllUsers = (state: RootState) => state.users;
+const selectUserById = (id: string | undefined) => (state: RootState) => state.users.find(u => u.id === id);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from './counter'
 import postReducer from './features/post/postSlice'
+import usersReducer from './features/users/usersSlice'
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
+    users: usersReducer,
   },
 });
 
