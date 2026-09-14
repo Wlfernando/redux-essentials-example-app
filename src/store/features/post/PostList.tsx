c32bcd9fd67996c64d96d3f5dbd8346807e34c74
Fernando Alcázar Malfavón <4maferal@gmail.com>
2026-09-14 17:41:52 -0600

    add async PostList

--- src/store/features/post/PostList.tsx
+++ src/store/features/post/PostList.tsx
@@ -1,27 +1,34 @@
-import { fetchPosts, selectPost, selectPostsStatus } from "@/store/features/post/postSlice";
+import { fetchPosts, selectPost, selectPostsError, selectPostsStatus } from "@/store/features/post/postSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { Link } from "react-router-dom";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "@/components/TimeAgo";
 import ReactionButtons from "./ReactionButtons";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { Spinner } from "@/components/Spinner";
 
 export default function PostsList() {
   const posts = useAppSelector(selectPost);
   const status = useAppSelector(selectPostsStatus);
-  const dispatch = useAppDispatch();
+  const error = useAppSelector(selectPostsError);
 
-  const reversePosts = posts.toSorted((a, b) => b.date.localeCompare(a.date))
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (status === 'idle')
       dispatch(fetchPosts())
   }, [status, dispatch])
 
-  return <>
-    <section className="posts-list">
-      <h2>Post</h2>
-      {reversePosts.map(p => (
+  let content: React.ReactNode = null;
+
+  if (status === 'pending') {
+    content = <Spinner text="loading..." />
+  }
+
+  else if (status === 'succeeded') {
+    const reversePosts = posts.toSorted((a, b) => b.date.localeCompare(a.date))
+    
+    content = reversePosts.map(p => (
         <article key={p.id} className="post-excerpt">
           <h3>
             <Link to={'/post/' + p.id}>{p.title}</Link>
@@ -31,7 +38,15 @@ export default function PostsList() {
           <TimeAgo timestamp={p.date} />
           <ReactionButtons post={p}/>
         </article>
-      ))}
+      ))
+  } else if (status === 'failed') {
+    content = <div>{ error }</div>
+  }
+
+  return <>
+    <section className="posts-list">
+      <h2>Post</h2>
+      { content }
     </section>
   </>
 }
--- src/store/features/post/postSlice.ts
+++ src/store/features/post/postSlice.ts
@@ -125,5 +125,6 @@ export default postSlice.reducer;
 export const selectPost = (state: RootState) => state.posts.posts;
 export const selectAPost = (id: string | undefined) => (state: RootState) => state.posts.posts.find(post => post.id === id)
 export const selectPostsStatus = (state: RootState) => state.posts.status;
+export const selectPostsError = (state: RootState) => state.posts.error;
 
 export const { addPost, postUpdated, reactionAdded } = postSlice.actions;
