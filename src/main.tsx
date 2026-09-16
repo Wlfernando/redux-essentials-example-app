6d7916f3e2a9ac494a2852e6749f52588d4d9663
Fernando Alcázar Malfavón <4maferal@gmail.com>
2026-09-15 21:31:06 -0600

    fetch fake users

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,14 @@ import './index.css'
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { fetchUsers } from './store/features/users/usersSlice';
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
+
+  store.dispatch(fetchUsers())
 
   const root = createRoot(document.getElementById('root')!)
 
--- src/store/features/users/usersSlice.ts
+++ src/store/features/users/usersSlice.ts
@@ -1,17 +1,29 @@
 import { RootState } from "@/store";
 import { createSlice } from "@reduxjs/toolkit";
 import { selectUserName } from "../auth/authSlice";
+import { createAppAsyncThunk } from "@/store/withTypes";
+import { client } from "@/api/client";
 
 interface User {
   id: string;
   name: string;
 }
 
-const initialState: User[] = [
-  { id: '0', name: 'Tianna Jenkins' },
-  { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
-];
+// const initialState: User[] = [
+//   { id: '0', name: 'Tianna Jenkins' },
+//   { id: '1', name: 'Kevin Grant' },
+//   { id: '2', name: 'Madison Price' },
+// ];
+
+export const fetchUsers = createAppAsyncThunk(
+  'users/fetchUsers',
+  async () => {
+    const res = await client.get<User[]>('/fakeApi/users');
+    return res.data;
+  }
+)
+
+const initialState: User[] = [];
 
 const userSlice = createSlice({
   initialState,
@@ -19,6 +31,11 @@ const userSlice = createSlice({
   reducers: {
 
   },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    })
+  }
 });
 
 export default userSlice.reducer;
